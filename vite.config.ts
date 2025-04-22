902fbcd3eee1e0281020551929112f48e9be63e3
omniflow73 <achus3069@gmail.com>
2025-04-22 22:16:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+base: '/flowstate-command-center/'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -20,4 +20,3 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
-base: '/flowstate-command-center/'
